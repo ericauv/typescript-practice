c0ba8238702f3c406705ef484936110e2e347dfb
ericauv <eriauv17@gmail.com>
2020-01-03 12:02:44 -0500

    add compiled interfaces

---
+++ interfaces.js
@@ -0,0 +1,13 @@
+"use strict";
+function drawRectangle(options) {
+    var width = options.width;
+    var length = options.length;
+    if (options.height) {
+        var height = options.height;
+    }
+    // Draw logic
+}
+drawRectangle({
+    width: 50,
+    length: 40
+});
--- types.js
+++ types.js
@@ -12,3 +12,19 @@ var newCount = [1, 2, 3, 4];
 var anything = 4;
 anything = 'text';
 anything = false;
+// Returning nothing
+function greeter(name) {
+    console.log('Hello, ' + name);
+}
+// Enums
+var joystickStatus = 1;
+var Directions;
+(function (Directions) {
+    Directions[Directions["Up"] = 0] = "Up";
+    Directions[Directions["Down"] = 1] = "Down";
+    Directions[Directions["Left"] = 2] = "Left";
+    Directions[Directions["Right"] = 3] = "Right";
+})(Directions || (Directions = {})); // compiles to Up=0, Down=1,...
+if (joystickStatus === Directions.Left) {
+    // move player to the left
+}
